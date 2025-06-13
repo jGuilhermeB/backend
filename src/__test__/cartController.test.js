8a60d41260073a2d79f4ce7787adeed9b6cc9cca
unknown <josebarroso0887@digitalcollege.internal>
2025-06-13 16:59:06 -0300

    álteração da url da database para o render

---
+++ src/__test__/cartController.test.js
@@ -0,0 +1,33 @@
+const cartController = require('../controllers/cartController.js')
+const prisma = require('../config/prisma.js')
+
+describe("Controller - criar carrinho", () => {
+    let user;
+
+    beforeAll(async () => {
+        user = await prisma.user.findUnique({
+            where: { email: 'jm@exemplo.com' }
+        })
+
+    })
+    test('deve criar um carrinho com sucesso', async () => {
+        const req = {
+            user: { id: user.id },
+        }
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+
+        }
+        await cartController.criarCarrinho(req, res)
+        expect(res.status).toHaveBeenCalledWith(201)
+        expect(res.json).toHaveBeenCalledWith({message: "Carrinho criado com sucesso",
+
+             })
+            const carrinho = await prisma.cart.findFirst({
+                where : {user_id : user.id},
+            })
+        expect (carrinho).not.toBeNull()
+    })
+
+})
