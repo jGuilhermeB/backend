06e3ec756cf48b1dcc1fe35a3dbe00e6107a2abb
unknown <josebarroso0887@digitalcollege.internal>
2025-06-13 17:07:17 -0300

    colocando methods

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors({
-   origin: "http://localhost:5173"
+   origin: "https://g-tsprint2.vercel.app/",
+   methods:["GET", "POST", "PUT", "DELETE"],
+   allowedHeaders: ["Content-type", "Authorization"]
+   
 }))
 
 app.use(express.json())
